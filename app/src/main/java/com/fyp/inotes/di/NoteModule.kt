9ef533954103c3758e356424ef461bab66da3f1a
nourr <nourmorgan19@gmail.com>
2023-05-28 00:45:41 +0300

    setup di

---
+++ app/src/main/java/com/fyp/inotes/di/NoteModule.kt
@@ -0,0 +1,45 @@
+package com.fyp.inotes.di
+
+import android.app.Application
+import androidx.room.Room
+import com.fyp.inotes.feature_note.data.local.NoteDatabase
+import com.fyp.inotes.feature_note.data.local.NoteDatabase.Companion.DATABASE_NAME
+import com.fyp.inotes.feature_note.data.repository.RepositoryImpl
+import com.fyp.inotes.feature_note.domain.repostory.Repository
+import com.fyp.inotes.feature_note.domain.usecase.DeleteNoteUseCase
+import com.fyp.inotes.feature_note.domain.usecase.GetNotesUsecase
+import com.fyp.inotes.feature_note.domain.usecase.NoteUseCases
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@InstallIn(Singleton::class)
+object NoteModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(@ApplicationContext app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            context = app,
+            klass = NoteDatabase::class.java,
+            name = DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(database: NoteDatabase): Repository {
+        return RepositoryImpl(database.dao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUsecases(repository: Repository): NoteUseCases {
+        return NoteUseCases(
+            getNotesUsecase = GetNotesUsecase(repository),
+            deleteNoteUseCase = DeleteNoteUseCase(repository)
+        )
+    }
+
+}
--- app/src/main/java/com/fyp/inotes/feature_note/data/local/NoteDatabase.kt
+++ app/src/main/java/com/fyp/inotes/feature_note/data/local/NoteDatabase.kt
@@ -7,4 +7,8 @@ import com.fyp.inotes.feature_note.domain.model.Note
 @Database(entities = [Note::class], version = 1)
 abstract class NoteDatabase : RoomDatabase() {
     abstract val dao: NoteDao
+
+    companion object {
+        const val DATABASE_NAME = "notes_db"
+    }
 }
